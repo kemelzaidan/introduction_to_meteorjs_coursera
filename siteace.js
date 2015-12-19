8e30088dc7ba369dc4d6d48d6326be7aef97c33e
Kemel Zaidan <kemelzaidan@gmail.com>
2015-12-19 02:17:53 -0200

    exercise 3: up and down vote

--- siteace.js
+++ siteace.js
@@ -23,8 +23,12 @@ if (Meteor.isClient) {
 			// example of how you can access the id for the website in the database
 			// (this is the data context for the template)
 			var website_id = this._id;
+			var site_rating = this.rating;
 			console.log("Up voting website with id "+website_id);
 			// put the code in here to add a vote to a website!
+			Websites.update({_id:website_id},
+											{$inc: {rating:1}});
+			console.log("Rating of "+site_rating);
 
 			return false;// prevent the button from reloading the page
 		},
@@ -33,9 +37,13 @@ if (Meteor.isClient) {
 			// example of how you can access the id for the website in the database
 			// (this is the data context for the template)
 			var website_id = this._id;
+			var site_rating = this.rating;
 			console.log("Down voting website with id "+website_id);
 
 			// put the code in here to remove a vote from a website!
+			Websites.update({_id:website_id},
+											{$inc: {rating:-1}});
+			console.log("Rating of "+site_rating);
 
 			return false;// prevent the button from reloading the page
 		}
@@ -58,8 +66,9 @@ if (Meteor.isClient) {
 			Websites.insert({url:url,
 											title:title,
 											description:description,
-											createdOn: new Date(),
-											createdBy: Meteor.user()._id
+											createdOn:new Date(),
+											createdBy:Meteor.user()._id,
+											rating:0
 										});
 
 			$("#website_form").toggle('slow');
@@ -71,35 +80,5 @@ if (Meteor.isClient) {
 
 
 if (Meteor.isServer) {
-	// start up function that creates entries in the Websites databases.
-  Meteor.startup(function () {
-    // code to run on server at startup
-    if (!Websites.findOne()){
-    	console.log("No websites yet. Creating starter data.");
-    	  Websites.insert({
-    		title:"Goldsmiths Computing Department",
-    		url:"http://www.gold.ac.uk/computing/",
-    		description:"This is where this course was developed.",
-    		createdOn:new Date()
-    	});
-    	 Websites.insert({
-    		title:"University of London",
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-    		description:"University of London International Programme.",
-    		createdOn:new Date()
-    	});
-    	 Websites.insert({
-    		title:"Coursera",
-    		url:"http://www.coursera.org",
-    		description:"Universal access to the world’s best education.",
-    		createdOn:new Date()
-    	});
-    	Websites.insert({
-    		title:"Google",
-    		url:"http://www.google.com",
-    		description:"Popular search engine.",
-    		createdOn:new Date()
-    	});
-    }
-  });
+
 }

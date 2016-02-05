7c458de4858594c18c70746985cf8112d0850d66
Kemel Zaidan <kemelzaidan@gmail.com>
2016-02-04 23:01:07 -0200

    votes tokenized and on db

--- siteace.js
+++ siteace.js
@@ -61,7 +61,7 @@ if (Meteor.isClient) {
 			var title = this.title.toLowerCase();
 			var regex = /[a-z\u00E0-\u00FC]{3,}/g; // grab just words >= 3 letters
 			// create an array of undesired words
-			var dontWant = ['i', 'a', 'an', 'am', 'the', 'this', 'that', 'those', 'then', 'than', 'you', 'he', 'she', 'us', 'we', 'were', 'it', 'was'];
+			var dontWant = ['i', 'a', 'an', 'am', 'the', 'this', 'that', 'those', 'then', 'than', 'you', 'he', 'she', 'us', 'we', 'were', 'it', 'was', 'for', 'at'];
 			// create an array of words out of title and description
 			var tokens = description.match(regex);
 			var tokens2= title.match(regex);
@@ -75,6 +75,22 @@ if (Meteor.isClient) {
 				return dontWant.indexOf(element) < 0;
 			});
 			console.log("Tokens are: ", tokens);
+			// convert tokens to votes object
+			var votes = {};
+			for (var i in tokens){
+				votes[tokens[i]] = 1;
+			}
+			console.log('Votes are: ', votes);
+			if (!Votes.findOne({userId: Meteor.userId()})) {
+				var votesID = Votes.insert({userId: Meteor.userId()});
+				Session.set('votesID', votesID);
+			} else {
+				var id = Votes.findOne({userId: Meteor.userId()});
+				Session.set('votesID', id._id);
+			}
+
+			Votes.upsert({_id: Session.get('votesID')}, {$inc: votes });
+
 
 			return false;// prevent the button from reloading the page
 		},
